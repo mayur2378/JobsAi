cf47ba33617f766e808fd7582479977e764592bc
Mayur Fotedar <mfotedar@gmail.com>
2026-05-03 21:55:20 -0500

    feat: log page views for authenticated users

---
+++ web/components/layout/PageViewLogger.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { usePathname } from 'next/navigation'
+import { useEffect } from 'react'
+
+export function PageViewLogger() {
+  const pathname = usePathname()
+  useEffect(() => {
+    fetch('/api/track', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ path: pathname }),
+    }).catch(() => {})
+  }, [pathname])
+  return null
+}
